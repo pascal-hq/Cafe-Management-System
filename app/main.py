91c9627d291d1f1b21a0009b6c63b3de3b64deed
MuthamaPK <pascalmuthama01@gmail.com>
2026-01-19 23:33:41 +0300

    Connecting login page to API

--- app/main.py
+++ app/main.py
@@ -6,10 +6,20 @@ from .routes import users   # Auth routes
 from .routes import menu    # Menu CRUD routes
 from .routes import orders  # Orders routes
 
+
 app = FastAPI(
     title="Cafe Management System",
     description="API for managing cafe orders, menu, and sales",
     version="1.0.0"
+)
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://127.0.0.1:5500"] if using VS Code Live Server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Create tables
